12991dda52d74eb418dccb89b844c3c7c9f44829
Vinod <vinod_882000@yahoo.com>
2022-03-23 22:01:26 -0400

    Revert "Revert "i changed my file which is modified at GIT HUB also""
    This reverts commit 686bd95019977ca969eba3b039e6c743f5e48a44.

--- RemoteFile1.java
+++ RemoteFile1.java
@@ -1,11 +1,5 @@
 I have created repository and added this first line from GIT HUB
 Add Second line from local
-<<<<<<< .mine
 Switch to enhancement branch and added third line
 Some one changed the code at GIT HUB at my local 
 When some one changed i have also done changes at my local which are not pushed
-=======
-Switch to enhancement branch and added third line
-
-
->>>>>>> .theirs
